d5bd21ad8d2329674a07506d103409cb9b4198bd
Calin Teodorescu <calinteodorescu@hotmail.com>
2025-06-10 04:06:09 -0400

    .

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,8 +1,20 @@
 set(CMAKE_SYSTEM_NAME Windows)
+
 set(CMAKE_C_COMPILER C:/Tools/msys64/mingw64/bin/gcc.exe)
 set(CMAKE_CXX_COMPILER C:/Tools/msys64/mingw64/bin/g++.exe)
-set(CMAKE_MAKE_PROGRAM C:/Tools/msys64/mingw64/bin/make.exe)
+set(CMAKE_MAKE_PROGRAM C:/Tools/msys64/mingw64/bin/cmake.exe)
 set(CMAKE_FIND_ROOT_PATH C:/Tools/msys64/mingw64)
+
+set(CMAKE_INSTALL_PREFIX D:/Git/ceph/.build/cmake/.run)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(WITH_SYSTEM_BOOST ON)
+set(WITH_RBD OFF)
+set(WITH_RADOSGW OFF)
+set(WITH_MANPAGE OFF)
+set(WITH_BABELTRACE OFF)
+set(WITH_MGR_DASHBOARD_FRONTEND OFF)
+
+D:/Git/ceph/.build/cmake/.build.out
